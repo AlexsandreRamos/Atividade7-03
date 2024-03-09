5b8e8b37722823b99662ff0736737191f13edc1c
AlexsandreRamos <161656902+AlexsandreRamos@users.noreply.github.com>
2024-03-08 21:10:52 -0300

    Aula07_03.java

--- src/main/java/com/mycompany/aula07_03/Aula07_03.java
+++ src/main/java/com/mycompany/aula07_03/Aula07_03.java
@@ -27,7 +27,7 @@ public class Aula07_03 {
       
         System.out.println("ATIVIDADE 1 \n");
       
-        System.out.println("Digite um número");
+        System.out.println("Digite um número de 1 a 10:");
               int num9 = teclado1.nextInt();
       
       while (num9 != 10){
